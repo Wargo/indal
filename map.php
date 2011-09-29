33d8e3ed546b1f4d4f019324f446170ba9d2498e
Guillermo <guillermo@artvisual.net>
2011-09-30 00:59:13 +0200

    contacto

---
+++ map.php
@@ -0,0 +1,37 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include('functions.php');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Contacto</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+</head>
+
+<?php
+$zones = array(
+	'america' => 'north_america',
+	'sudamerica' => 'south_america',
+	'africa' => 'africa',
+	'europa' => 'europe',
+	'asia' => 'asia',
+	'oceania' => 'oceania',
+);
+$aux = array();
+foreach($zones as $key => $value) {
+	 $aux[] = $key . '=results.php?zone=' . $value;
+}
+?>
+<body onload="initialize()" onunload="GUnload()">
+	<h1>Contacto</h1>
+	<?php include('form.php'); ?>
+	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="500" height="300">
+	<param name="movie" value="map.swf" />
+	<param name="quality" value="high" />
+	<embed src="map.swf?<?php echo implode('&', $aux); ?>" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="500" height="300"></embed>
+	</object>
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,118 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+include('functions.php');
+
+if(empty($_REQUEST['zone'])) {
+	$zone = 'europe';
+} else {
+	$zone = $_REQUEST['zone'];
+}
+
+$offices = array(
+	array(
+		'lat' => '39.673197',
+		'lng' => '-0.681023',
+		'name' => 'Oficina 1',
+	),
+	array(
+		'lat' => '43.673197',
+		'lng' => '9.681023',
+		'name' => 'Oficina 2',
+	),
+	array(
+		'lat' => '39.673197',
+		'lng' => '21.681023',
+		'name' => 'Oficina 3',
+	),
+	array(
+		'lat' => '56.673197',
+		'lng' => '9.681023',
+		'name' => 'Oficina 4',
+	),
+);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Contacto</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAzr2EBOXUKnm_jVnk0OJI7xSosDVG8KKPE1-m51RBrvYughuyMxQ-i1QfUnH94QxWIa6N4U6MouMmBA"
+	type="text/javascript"></script>
+	<script type="text/javascript">
+
+	var europe = new GLatLng(47, 7);
+	var north_america = new GLatLng(47, -120);
+	var south_america = new GLatLng(-20, -70);
+	var africa = new GLatLng(0, 0);
+	var asia = new GLatLng(55, 90);
+	var oceania = new GLatLng(-20, 135);
+
+	function initialize() {
+		if (GBrowserIsCompatible()) {
+			var map = new GMap2(document.getElementById("map_canvas"));
+			map.setCenter(<?php echo $zone; ?>, 2);
+
+			// Add 10 markers to the map at random locations
+			var bounds = map.getBounds();
+			var southWest = bounds.getSouthWest();
+			var northEast = bounds.getNorthEast();
+			var lngSpan = northEast.lng() - southWest.lng();
+			var latSpan = northEast.lat() - southWest.lat();
+			<?php
+			foreach($offices as $office) {
+				echo '
+				var point = new GLatLng(' . $office['lat'] . ', ' . $office['lng'] . ');
+				map.addOverlay(createMarker(point, \'' . $office['name'] . '\'));
+				';
+			}
+			?>
+			// Zoom
+			map.addControl(new GSmallMapControl());
+
+			// satelite, mapa, etc...
+			var mapTypeControl = new GMapTypeControl();
+			var topRight = new GControlPosition(G_ANCHOR_TOP_RIGHT, new GSize(10,10));
+			var bottomRight = new GControlPosition(G_ANCHOR_BOTTOM_RIGHT, new GSize(10,10));
+			map.addControl(mapTypeControl, topRight);
+
+		}
+	}
+	// Create a base icon for all of our markers that specifies the
+	// shadow, icon dimensions, etc.
+	var baseIcon = new GIcon(G_DEFAULT_ICON);
+	baseIcon.shadow = "http://www.google.com/mapfiles/shadow50.png";
+	baseIcon.iconSize = new GSize(20, 34);
+	baseIcon.shadowSize = new GSize(37, 34);
+	baseIcon.iconAnchor = new GPoint(9, 34);
+	baseIcon.infoWindowAnchor = new GPoint(9, 2);
+
+	// Función add marker
+	// Creates a marker whose info window displays the letter corresponding
+	// to the given index.
+	function createMarker(point, text) {
+		var letter = text;
+		var letteredIcon = new GIcon(baseIcon);
+		letteredIcon.image = "http://www.google.com/mapfiles/marker.png";
+
+		// Set up our GMarkerOptions object
+		markerOptions = { icon:letteredIcon };
+		var marker = new GMarker(point, markerOptions);
+
+		GEvent.addListener(marker, "click", function() {
+			marker.openInfoWindowHtml("<b>" + letter + "</b>");
+		});
+		return marker;
+	}
+
+	</script>
+</head>
+
+<body onload="initialize()" onunload="GUnload()">
+	<h1>Contacto</h1>
+	<?php include('form.php'); ?>
+	<div id="map_canvas" style="width: 500px; height: 400px; margin-top: 10px;"></div>
+</body>
+</html>
