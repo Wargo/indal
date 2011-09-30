52283625c544d94754758e24f40f7b6f7c9733c2
Guille <guillermo@artvisual.net>
2011-09-30 11:36:58 +0200

    zonas

--- map.php
+++ map.php
@@ -31,6 +31,11 @@ foreach($zones as $key => $value) {
 	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="500" height="300">
 	<param name="movie" value="map.swf" />
 	<param name="quality" value="high" />
+	<?php
+	foreach($zones as $key => $value) {
+		echo '<param name="' . $key . '" value="results.php?zone=' . $value . '" />';
+	}
+	?>
 	<embed src="map.swf?<?php echo implode('&', $aux); ?>" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="500" height="300"></embed>
 	</object>
 </body>
