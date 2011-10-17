416a3d3bde63e920a317a2341ab28cb96b5328de
Guille <guillermo@artvisual.net>
2011-10-17 14:03:13 +0200

    cambios

--- form.php
+++ form.php
@@ -9,11 +9,11 @@
 	<form method="post" action="results.php">
 		<select name="zone">
 			<option <?php if($zone == 'europe') echo 'selected'; ?> value="europe">Europe</option>
-			<option <?php if($zone == 'north_america') echo 'selected'; ?> value="north_america">North America</option>
+			<option disabled="true" <?php if($zone == 'north_america') echo 'selected'; ?> value="north_america">North America</option>
 			<option <?php if($zone == 'south_america') echo 'selected'; ?> value="south_america">South America</option>
 			<option <?php if($zone == 'asia') echo 'selected'; ?> value="asia">Asia</option>
 			<option <?php if($zone == 'africa') echo 'selected'; ?> value="africa">Africa</option>
-			<option <?php if($zone == 'oceania') echo 'selected'; ?> value="oceania">Oceania</option>
+			<option disabled="true" <?php if($zone == 'oceania') echo 'selected'; ?> value="oceania">Oceania</option>
 		</select>
 		<!--
 		<select name="country">
