b88ca65c7db48dffd5cce722aabf7261a93ace0b
Guille <guillermo@artvisual.net>
2011-11-03 01:25:42 +0100

    cambios

--- photos.php
+++ photos.php
@@ -17,8 +17,31 @@ $url = 'http://www.flickr.com/photos/'.$nsid.'/'; //Url de la Imgen Original
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Fotos</title>
 	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<script>
+	var time;
+	function show(event, url) {
+		//var y = event.screenY;
+		//var x = event.screenX;
+		if(document.getElementById('div').style.display == 'none') {
+			document.getElementById('image').src = '';
+			document.getElementById('div').style.display = '';
+			var y = event.pageY;
+			var x = event.pageX;
+			document.getElementById('div').style.left = x + 'px';
+			document.getElementById('div').style.top = y + 'px';
+			document.getElementById('image').src = url;
+			clearTimeout(time);
+			time = setTimeout('hide()', 5000);
+		}
+	}
+	function hide() {
+		clearTimeout(time);
+		document.getElementById('div').style.display = 'none';
+	}
+	</script>
 </head>
 <body>
+	<div style="display: none;" id="div"><img src="" border="0" id="image" width="400" /></div>
 	<div class="header">
 		<h1>Zona multimedia</h1>
 		<ul class="menu">
@@ -34,7 +57,7 @@ $url = 'http://www.flickr.com/photos/'.$nsid.'/'; //Url de la Imgen Original
 		if (is_array($photos['photo'])) {
 			foreach ($photos['photo'] as $photo) {
 				echo '<li class="image">';
-					echo '<a target="_blank" href="'.$url.$photo['id'].'"><img class="img" alt="'.$photo['title'].'" title="'.$photo['title'].'" src="'.$f->buildPhotoURL($photo, 'square').'" /></a>';
+					echo '<a onmouseout="hide();" onmouseover="show(event, \''.$f->buildPhotoURL($photo, 'large').'\');" target="_blank" href="'.$url.$photo['id'].'"><img class="img" alt="'.$photo['title'].'" title="'.$photo['title'].'" src="'.$f->buildPhotoURL($photo, 'square').'" /></a>';
 					echo '<a target="_blank" href="'.$url.$photo['id'].'" class="title">' . short_text($photo['title'], 25) . '</a>';
 				echo '</li>';
 				//<object style="height: 390px; width: 640px"><param name="movie" value="http://www.youtube.com/v/' . $id . '?version=3"><param name="allowFullScreen" value="true"><param name="allowScriptAccess" value="always"><embed src="http://www.youtube.com/v/' . $id . '?version=3" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always" width="640" height="360"></object>
