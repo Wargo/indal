6a0a14bce48ff96baef88a63235b22aca9ca05b3
Guille <guillermo@artvisual.net>
2012-01-16 09:05:03 +0100

    conexion

--- results.php
+++ results.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 1);
 
 include('functions.php');
 
-mysql_connect('localhost', 'root', 'root');
+//mysql_connect('localhost', 'root', 'root');
+mysql_connect('localhost', 'indal', 'dSoek9Xp');
 mysql_select_db('indal');
 
 if(empty($_POST['zone'])) {
